dcf440ee27cbf728ef0127f8dc3dc8c1c679a8ed
TheoVerhelst <tverhels@ulb.ac.be>
2017-04-19 22:56:09 +0200

    FIX: don't forget to convert from rectangular to polar representation

--- src/overpitch/time_scaling.clj
+++ src/overpitch/time_scaling.clj
@@ -15,7 +15,7 @@
 
 (defn hann-window
   "The hann window function is defined as 0.5*(1 - cos(2*pi*x)), for x in [0, 1].
-  It allows a smooth fading at the limits of a sound frame, and has nice
+  It allows for a smooth fading at the limits of a sound frame, and has nice
   mathematical properties.
   "
   [x]
@@ -35,14 +35,27 @@
         (fn [i x] (* (hann-window (/ i length)) x))
         frame))))
 
+(defn convert-rectangular-to-polar
+  "Converts a map of numbers from real-imaginary coordinates to polar
+  coordinates. The numbers argument must be a map containing a vector or values
+  labelled :real, and another vector of values labelled :imaginary."
+  [numbers]
+  (let [real-parts (:real numbers) imaginary-parts (:imaginary number)
+        magnitudes (mapv #(Math/hypot (? 0) (? 1)) real-parts imaginary-parts)
+        ; Watch out, atan2 takes y (the imaginary part) as first argument
+        phases (mapv #(Math/atan2 (? 1) (? 0)) real-parts imaginary-parts))
+    {:magnitudes magnitudes :phases phases})
+
 (defn fft
   [frame]
   (let [result (double-array (* 2 frame-size) frame)]
     ; call realForwardFull method on the jtransforms fft object, with result as
     ; argument. The array result will be overwritten by the method
     (.realForwardFull jtransforms-fft-instance result)
-    ; Now result contains real and imaginary values, return them in a map
-    (zipmap [:magnitudes :phases] (utils/split-channels result 2))))
+    ; Now result contains real and imaginary values, put it in a map and convert
+    ; it to polar coordinates
+    (convert-rectangular-to-polar
+      (zipmap [:real :imaginary] (utils/split-channels result 2)))))
 
 (defn ifft
   [magnitudes phases]
